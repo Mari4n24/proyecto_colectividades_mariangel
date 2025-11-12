a815a67a3e06c311f3f47818c1e942f2c377afeb
Mari <marur00911@gmail.com>
2025-11-12 17:57:34 -0300

    Agregar vista de inicio y plantilla HTML para la página de inicio

--- seguimiento/settings.py
+++ seguimiento/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'seguimiento.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\proyecto_colectividades_mariangel\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

441add77666e0512d3116cc75155652397ee4a4b
Mari <marur00911@gmail.com>
2025-11-18 11:13:55 -0300

    Agregar vista y plantilla para la creación de colectividades

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from inicio.views import inicio
+from inicio.views import *
 
 urlpatterns = [
     path('', inicio),
+    path('crear-colectividad/<nombre>/<pais>/', crear_colectividad)
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from inicio.models import Colectividad
 
 def inicio(request):
     return render(request, 'inicio.html')
+
+def crear_colectividad(request, nombre, pais):
+    
+    colectividades = Colectividad(nombre=nombre, pais=pais)
+    colectividades.save()
+    
+    return render(request, 'crear_colectividad.html')

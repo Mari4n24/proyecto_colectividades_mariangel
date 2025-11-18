33f45b063addc70e62bf43aa75ac024e5190c6f0
Mari <marur00911@gmail.com>
2025-11-18 11:35:32 -0300

    Agregar método __str__ a la clase Colectividad y mejorar la vista de listado de colectividades

--- inicio/models.py
+++ inicio/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Colectividad(models.Model):
     nombre = models.CharField(max_length=30)
     pais = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return f'Colectividad ({self.id}) : {self.nombre} - {self.pais}'

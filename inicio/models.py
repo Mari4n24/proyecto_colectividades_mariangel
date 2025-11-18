57c24bb7ad2ea535a5439793f1c1af1562e0ec04
Mari <marur00911@gmail.com>
2025-11-18 10:55:15 -0300

    Agregar modelo 'Colectividad' y registrar la aplicación 'inicio' en la configuración

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Colectividad(models.Model):
+    nombre = models.CharField(max_length=30)
+    pais = models.CharField(max_length=30)
